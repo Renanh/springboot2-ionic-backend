ef878872d1ebecec9bfbf0a3fb3457915b3d1f47
Renanh <renanh.Lima@gmail.com>
2018-12-03 21:57:00 +0000

    Estado e cidade

--- src/main/java/com/nelioalves/cursomc/domain/Produto.java
+++ src/main/java/com/nelioalves/cursomc/domain/Produto.java
@@ -39,7 +39,6 @@ public class Produto implements Serializable {
 	}
 
 	public Produto(Integer id, String nome, Double preco) {
-		super();
 		this.id = id;
 		this.nome = nome;
 		this.preco = preco;
