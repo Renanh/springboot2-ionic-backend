c8d0796d20e3e84621a760d5ad46ee099593e32b
Renanh <renanh.Lima@gmail.com>
2018-12-04 21:58:00 +0000

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -50,6 +50,10 @@ public class Cliente implements Serializable {
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 
+	@JsonManagedReference
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+
 	public Cliente() {
 	}
 
@@ -93,14 +97,6 @@ public class Cliente implements Serializable {
 		this.cpfOuCnpj = cpfOuCnpj;
 	}
 
-//	public TipoCliente getTipo() {
-//		return TipoCliente.toEnum(tipo);
-//	}
-//
-//	public void setTipo(TipoCliente tipo) {
-//		this.tipo = tipo.getCod();
-//	}
-
 	public TipoCliente getTipo() {
 		return tipo;
 	}
@@ -123,6 +119,14 @@ public class Cliente implements Serializable {
 
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 
 	@Override
---
+++ src/main/java/com/nelioalves/cursomc/domain/PagamentoComBoleto.java
@@ -0,0 +1,47 @@
+package com.nelioalves.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+import com.nelioalves.cursomc.domain.enums.EstadoPagamento;
+
+@Entity
+public class PagamentoComBoleto extends Pagamento implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Column(name = "DATA_VENCIMENTO")
+	private Date dataVencimento;
+
+	@Column(name = "DATA_PAGAMENTO")
+	private Date dataPagamento;
+
+	public PagamentoComBoleto() {
+	}
+
+	public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido, Date dataVencimento,
+			Date dataPagamento) {
+		super(id, estado, pedido);
+		this.dataVencimento = dataVencimento;
+		this.dataPagamento = dataPagamento;
+	}
+
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+
+	public Date getDataPagamento() {
+		return dataPagamento;
+	}
+
+	public void setDataPagamento(Date dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+}
